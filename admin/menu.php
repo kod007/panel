f229a83579a3002b70a9305037471512fafe4554
kod007 <bahanem01@hotmail.com>
2025-10-09 19:30:08 +0300

    Add navigation menu to admin panel

---
+++ admin/menu.php
@@ -0,0 +1,29 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Anasayfa</a></li>
+        <li><a href="#">Kullanıcılar</a></li>
+        <li><a href="#">Ayarlar</a></li>
+        <li><a href="logout.php">Çıkış Yap</a></li>
+    </ul>
+</nav>
+<style>
+nav ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    background: #333;
+    display: flex;
+}
+nav ul li {
+    margin: 0;
+}
+nav ul li a {
+    display: block;
+    padding: 12px 20px;
+    color: #fff;
+    text-decoration: none;
+}
+nav ul li a:hover {
+    background: #444;
+}
+</style>
